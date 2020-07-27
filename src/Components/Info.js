990dd05aa7a092249e22368ff59e458f65c50cfa
Gopisetti Lahari <40907986+GOPISETTILAHARI@users.noreply.github.com>
2020-07-27 07:59:59 +0530

    Add files via upload

---
+++ src/Components/Info.js
@@ -0,0 +1,15 @@
+import React from "react";
+const Info = () => {
+  return (
+    <div className="container">
+      <h1> WELCOME</h1>
+      <h5 style={{ color: "green" }}>
+        click on <b>Home</b> to add your topics
+      </h5>
+      <h5 style={{ color: "blue" }}>
+        click on <b>Topics</b> to get the stored ones
+      </h5>
+    </div>
+  );
+};
+export default Info;
